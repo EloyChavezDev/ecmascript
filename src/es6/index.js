baf53e16e5aa62512659433971580f50c4aeceec
J. Eloy Chavez <laneloychavez@gmail.com>
2021-08-10 12:37:14 -0500

    V 1.0
    Ejemplos de como se hacen las cosas en ES5 y ahora en ES6

---
+++ src/es6/index.js
@@ -0,0 +1,146 @@
+function newfunction (name, age, country){
+    var name = name || 'oscar';
+    var age = age || 32;
+    var country = country || 'MX';
+    console.log (name, age, country);
+}
+
+// es6
+function newfunction2 (name ='Oscar', age = 32, country = "MX"){
+    console.log (name, age, country);
+}
+
+newfunction2();
+newfunction2('Ricardo', '23', 'CO');
+
+let hello = "Hello";
+let world = "World";
+let epicPhrase = hello + ' ' + world;
+console.log (epicPhrase);
+
+// es6
+let epicPhrase2 = `${hello} ${world}`;
+console.log(epicPhrase2); 
+
+let lorem = "Lorem Ipsum is simply dummy text of the printing and typesetting industry \n"
++ "otra frase"
+
+// es6
+let lorem2 = `otra frase epica que necesitamos
+ahora es otra frase
+`;
+
+console.log(lorem);
+console.log(lorem2);
+
+let person = {
+    'name': 'oscar',
+    'age': 32,
+    'country': 'MX'
+}
+
+console.log(person.name, person.age);
+
+// es6
+let {name, age, country}= person;
+console.log(name, age, country);
+
+let team1 = ['Oscar', 'Julian', 'Ricardo'];
+let team2 = ['Valeria', 'Yesica', 'Camila'];
+
+let education = ['David', ...team1, ...team2];
+
+console.log(education);
+
+{
+    var globalVar = "Global Var";
+}
+
+{
+    let globalLet = 'Global Let';
+    console.log(globalLet);
+}
+
+console.log(globalVar);
+
+const a = 'b';
+a = 'a';
+console.log(a);
+
+let name = "oscar";
+let age = 32;
+
+//es5
+obj = { name: name, age: age};
+//es6
+obj2 ={name, age};
+console.log(obj2);
+
+const names =[
+    {name: 'Oscar', age: 32}
+    {name: 'Yesica', age:27}
+]
+
+let listOfNames = names.map(function (item){
+    console.log(item.name);
+})
+
+let listOfNames2 = names.map(item => console.log(item.name));
+
+const listOfNames3 = (name, age, country) => {
+    ...
+}
+
+const listOfNames4 = name => {
+    ...
+}
+
+const square = num => num * num;
+
+const helloPromise = () => {
+    return new Promise ((resolve, reject) => {
+        if(false){
+            resolve('Hey!');
+        }else {
+            reject('Ups!!');
+        }
+    });
+}
+
+helloPromise()
+.then(response =>console.log(response))
+.then(()=>console.log('hola'))
+.catch(error => console.log(error));
+
+class calculator {
+    constructor(){
+        this.valueA = 0;
+        this.valueB = 0;
+    }
+    sum(valueA, valueB){
+        this.valueA = valueA;
+        this.valueB = valueB;
+        return this.valueA + this.valueB;
+    }
+}
+
+const calc = new calculator();
+console.log(calc.sum(2,2));
+
+import{ hello } from './module';
+
+hello();
+
+function* helloWorld(){
+    if(true){
+        yield 'Hello, ';
+    }
+    if(true){
+        yield 'World';
+    }
+};
+
+const genereatorHello = helloWorld();
+console.log(genereatorHello.next().value);
+console.log(genereatorHello.next().value);
+console.log(genereatorHello.next().value);
